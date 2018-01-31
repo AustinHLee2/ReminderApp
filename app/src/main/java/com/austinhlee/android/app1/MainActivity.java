abed9abcc52f5a5c3170c4629c4bdc371f2defd8
Austin Lee <austinhlee2@gmail.com>
2018-01-30 17:34:31 -0800

    Removed Floating Action Button and added options menu

--- app/src/main/java/com/austinhlee/android/app1/MainActivity.java
+++ app/src/main/java/com/austinhlee/android/app1/MainActivity.java
@@ -6,12 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,20 +25,43 @@ public class MainActivity extends AppCompatActivity {
 
         updateUI();
 
-        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.mainFAB);
-        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
     public void onResume(){
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_sort:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.action_options:
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     private void updateUI(){
@@ -54,5 +74,6 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new MyAdapter(myDataset.getTasks());
         mRecyclerView.setAdapter(mAdapter);
+
     }
 }
